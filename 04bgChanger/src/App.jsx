06a4b565a0589dbec946ab40a7d973e2700dc2e4
NeetuSarkar <neetusarkarmartin9@gmail.com>
2024-04-12 01:55:03 +0530

    new

---
+++ 04bgChanger/src/App.jsx
@@ -0,0 +1,63 @@
+import { useState } from "react";
+
+function App() {
+  const [color, setColor] = useState("olive");
+
+  return (
+    <>
+      <div
+        className="w-full h-screen duration-200"
+        style={{ backgroundColor: color }}
+      >
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+          <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+            <button
+              onClick={() => setColor("red")}
+              className="oultine-none px-4 py-1 rounded-full text-white shadow-sm"
+              style={{ backgroundColor: "red" }}
+            >
+              Red
+            </button>
+            <button
+              onClick={() => setColor("orange")}
+              className="oultine-none px-4 py-1 rounded-full text-white shadow-sm"
+              style={{ backgroundColor: "orange" }}
+            >
+              Orange
+            </button>
+            <button
+              onClick={() => setColor("green")}
+              className="oultine-none px-4 py-1 rounded-full text-white shadow-sm"
+              style={{ backgroundColor: "green" }}
+            >
+              Green
+            </button>
+            <button
+              onClick={() => setColor("blue")}
+              className="oultine-none px-4 py-1 rounded-full text-white shadow-sm"
+              style={{ backgroundColor: "blue" }}
+            >
+              Blue
+            </button>
+            <button
+              onClick={() => setColor("yellow")}
+              className="oultine-none px-4 py-1 rounded-full text-white shadow-sm"
+              style={{ backgroundColor: "yellow" }}
+            >
+              Yellow
+            </button>
+            <button
+              onClick={() => setColor("pink")}
+              className="oultine-none px-4 py-1 rounded-full text-white shadow-sm"
+              style={{ backgroundColor: "pink" }}
+            >
+              Pink
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
